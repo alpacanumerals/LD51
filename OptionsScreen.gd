90e5851fe851dcc1385b0aa793e497d0e42614df
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2022-09-30 21:00:27 +0100

    wire up volume sliders

--- OptionsScreen.gd
+++ OptionsScreen.gd
@@ -1,12 +1,23 @@
 extends Node
 
 func _ready():
-    pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+    var sound_volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Sound"))
+    var sound_slider = get_node("%SoundSlider")
+    sound_slider.set_value(sound_volume)
+    var music_volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music"))
+    var music_slider = get_node("%MusicSlider")
+    music_slider.set_value(music_volume)
 
 func _on_BackButton_pressed():
     switcher.playClick()
     switcher.switchScene("res://Title.tscn")
+
+func _on_SoundSlider_value_changed(value):
+    var volume = clamp(value, -25, 15)
+    AudioServer.set_bus_volume_db(
+        AudioServer.get_bus_index("Sound"), volume)
+
+func _on_MusicSlider_value_changed(value):
+    var volume = clamp(value, -25, 15)
+    AudioServer.set_bus_volume_db(
+        AudioServer.get_bus_index("Music"), volume)
