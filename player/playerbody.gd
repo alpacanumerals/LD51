d2a0edc0af1317b4048c74f039379897b9d35fde
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2022-10-01 10:46:43 +0100

    get direction

--- player/playerbody.gd
+++ player/playerbody.gd
@@ -3,11 +3,12 @@ extends KinematicBody2D
 export (int) var speed = 200
 
 var velocity = Vector2()
+var direction = Vector2()
 
 signal shoot(bullet, direction, location)
-var Bullet = preload("res://Bullet.tscn")
+var Bullet = preload("res://bullet/Bullet.tscn")
 
-func get_input():
+func process_movement_input():
     velocity = Vector2()
     if Input.is_action_pressed("ui_right"):
         velocity.x += 1
@@ -18,12 +19,14 @@ func get_input():
     if Input.is_action_pressed("ui_up"):
         velocity.y -= 1
     velocity = velocity.normalized() * speed
-
-func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
     
 func _input(event):
     if event is InputEventMouseButton:
         if event.button_index == BUTTON_LEFT and event.pressed:
-            emit_signal("shoot", Bullet, rotation, position)
+            emit_signal("shoot", Bullet, direction, position)
+
+func _physics_process(delta):
+    process_movement_input()
+    velocity = move_and_slide(velocity)
+    
+    direction = (get_global_mouse_position() - position).normalized()
