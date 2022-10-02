28f1358e8b3eb5af7d0487c2c7a2899e5c281015
ruruie <58190249+Ruruie-FGSC@users.noreply.github.com>
2022-10-02 02:28:26 -0400

    UI

--- autoload/switcher.gd
+++ autoload/switcher.gd
@@ -8,10 +8,12 @@ func _ready():
 func switch_scene(path):
     yield(get_tree().create_timer(0.15), "timeout")
     get_tree().change_scene(path)
+    get_tree().paused = false
     
 func switch_scene_long(path):
     yield(get_tree().create_timer(0.5), "timeout")
     get_tree().change_scene(path)
+    get_tree().paused = false
     
 func switch_quit():
     yield(get_tree().create_timer(0.20), "timeout")
--- level/pausebox.gd
+++ level/pausebox.gd
@@ -9,14 +9,40 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pause_mode = Node.PAUSE_MODE_PROCESS
-    get_tree().paused = true    
-    
-func _input(event):
-    if event is InputEventMouseButton:
-        if event.button_index == BUTTON_RIGHT and event.pressed:
-            get_tree().paused = false
-            queue_free()
+    get_tree().paused = true
+    yield(get_tree().create_timer(0.20), "timeout")
+    music.Orchestrion.set_stream_paused(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #    pass
+
+func _on_Unpause_mouse_entered():
+    sounds.sfx_mo()
+
+func _on_Full_Screen_mouse_entered():
+    sounds.sfx_mo()
+
+func _on_Main_Menu_mouse_entered():
+    sounds.sfx_mo()
+
+func _on_Unpause_pressed():
+    sounds.sfx_sel()
+    music.Orchestrion.set_stream_paused(false)
+    yield(get_tree().create_timer(0.20), "timeout")
+    get_tree().paused = false
+    queue_free()
+
+func _on_Full_Screen_pressed():
+    sounds.sfx_sel()
+    yield(get_tree().create_timer(0.15), "timeout")
+    if OS.window_fullscreen == false:
+        OS.set_window_fullscreen(true)
+    else:
+        OS.set_window_fullscreen(false)
+
+func _on_Main_Menu_pressed():
+    sounds.sfx_sel()
+    music.Orchestrion.stop()
+    switcher.switch_scene("res://Title.tscn")
+    queue_free()
