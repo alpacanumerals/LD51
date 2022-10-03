19e17dab7f33c73e0a5196cf9aec9ecc6039f370
ruruie <58190249+Ruruie-FGSC@users.noreply.github.com>
2022-10-03 03:25:38 -0400

    MAP GEN

--- level/map.gd
+++ level/map.gd
@@ -1,25 +1,46 @@
 extends TileMap
 
-var map_blocks = [
-    blocks.block_1, blocks.block_1, blocks.block_1, blocks.block_1, blocks.block_1, blocks.block_1]
+var map_blocks = []
 
 var fallback_blocks = [
-    blocks.block_1, blocks.block_1, blocks.block_1, blocks.block_1, blocks.block_1, blocks.block_1]
+    blocks.block_0, blocks.block_0, blocks.block_0, blocks.block_0, blocks.block_0, blocks.block_0]
 
 func _ready():
     draw_map()
 
 func draw_map():
+    map_blocks.clear()
     var block_options = blocks.blocks
-    
+    var dead_options = blocks.blocks_dead
+    var start_options = blocks.blocks_start
+    var blockx = 5
+    var deadx = 0
+    var startx = 1
+    var rando
     # the map has 6 'blocks' used for random gen in a 3x2 grid. each is 8x8 tiles
-    
+    # gonna hardcode 6 here for now
     # picks a random block from the options for each block to start
-    for n in range(map_blocks.size()):
-        map_blocks[n] = block_options[randi() % block_options.size()]
     
+    #Now the Mal strategy kicks in. We budget out the blocks:
+    rando = rng.rng.randi_range(1,2)
+    if rando == 2:
+        blockx -= 1
+        deadx += 1
+    rando = rng.rng.randi_range(1,3)
+    if rando > 1:
+        blockx -= 1
+        startx += 1
+    
+    for n in range(blockx):
+        map_blocks.append(block_options[randi() % block_options.size()]) 
+    for n in range(deadx):
+        map_blocks.append(dead_options[randi() % dead_options.size()]) 
+    for n in range(startx):
+        map_blocks.push_back(start_options[randi() % start_options.size()]) 
+    map_blocks.shuffle()
     # this checks if the blocks form a valid map
     # if not it generates another map
+    # This should now always be valid.
     var iterations = 1
     while !validate_blocks() && iterations < 64:
         for n in range(map_blocks.size()):
@@ -81,6 +102,7 @@ func get_mob_starts():
     for n in map_blocks.size():
         var x_offset = (n % 3) * 8
         var y_offset = int(n / 3) * 8
+        print(map_blocks[n][9])
         for spawn in map_blocks[n][9]:
             var x = spawn[0] + x_offset
             var y = spawn[1] + y_offset
