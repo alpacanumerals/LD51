0e0cd31074b1bf6443616f664d70b5762f652a90
ruruie <58190249+Ruruie-FGSC@users.noreply.github.com>
2022-10-03 13:49:56 -0400

    push

--- power_ups/spd_up.gd
+++ power_ups/spd_up.gd
@@ -3,7 +3,7 @@ extends Area2D
 func _ready():
     add_to_group(constants.POW_GROUP)
 
-func _on_RoFUp_body_entered(body):
-    if body.has_method("rof_up"):
-        body.rof_up()
+func _on_SpdUp_body_entered(body):
+    if body.has_method("spd_up"):
+        body.atk_up()
         call_deferred("queue_free")
