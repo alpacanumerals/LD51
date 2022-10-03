1aefa54ad8d8ca3a67705a76419ef92fd6071d6d
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2022-10-03 16:56:16 +0100

    fix p_ups

--- level/p_up_display.gd
+++ level/p_up_display.gd
@@ -11,14 +11,23 @@ func add_atk_up():
     $AtkUps.add_child(icon)
 
 func add_rof_up():
-    var icon = get_p_up_icon(atk_up)
-    $AtkUps.add_child(icon)
+    var icon = get_p_up_icon(rof_up)
+    $RofUps.add_child(icon)
 
 func add_spd_up():
-    var icon = get_p_up_icon(atk_up)
-    $AtkUps.add_child(icon)
+    var icon = get_p_up_icon(spd_up)
+    $SpdUps.add_child(icon)
 
 func get_p_up_icon(image):
     var icon = icon_base.instance()
     icon.texture = image
     return icon
+
+func _on_PlayArea_atk_up():
+    add_atk_up()
+
+func _on_PlayArea_rof_up():
+    add_rof_up()
+
+func _on_PlayArea_spd_up():
+    add_spd_up()
