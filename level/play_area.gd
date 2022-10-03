8a0b15986ee13ae026a055aec91aea99979b4761
ruruie <58190249+Ruruie-FGSC@users.noreply.github.com>
2022-10-03 13:28:02 -0400

    fwoosh

--- level/play_area.gd
+++ level/play_area.gd
@@ -9,17 +9,21 @@ signal atk_up
 signal rof_up
 signal spd_up
 
+
 const tentacle = preload("res://mobs/Tentacle.tscn")
 const blob = preload("res://mobs/Blob.tscn")
 const dome = preload("res://mobs/Dome.tscn")
+
 const orange = preload("res://mobs/Orange.tscn")
 const amogus = preload("res://mobs/Amogus.tscn")
 const flower = preload("res://mobs/Flower.tscn")
-const greenghost = preload("res://mobs/GreenGhost.tscn")
+const ghost = preload("res://mobs/GreenGhost.tscn")
+
 const tendrils = preload("res://mobs/Tendrils.tscn")
-const timespider = preload("res://mobs/TSpider.tscn")
+const time = preload("res://mobs/TSpider.tscn")
 const prism = preload("res://mobs/Prism.tscn")
 const floater = preload("res://mobs/Floater.tscn")
+
 
 const atk_up = preload("res://power_ups/atk_up.tscn")
 const rof_up = preload("res://power_ups/rof_up.tscn")
@@ -29,9 +33,38 @@ const power_ups = [atk_up, rof_up]
 var difficulty = 1
 var current_mobs
 
-const pack_1 = [tentacle, blob, blob]
-const pack_2 = [tentacle, tentacle, blob]
-const pack_3 = [dome, dome, dome]
+const pack_1 = [tentacle,tentacle]
+const pack_2 = [blob,blob]
+const pack_3 = [dome,dome]
+const pack_4 = [blob,blob,blob]
+const pack_5 = [tentacle,tentacle,tentacle]
+const pack_6 = [dome,dome,dome]
+const pack_7 = [blob,blob,tentacle,tentacle]
+const pack_8 = [blob,blob,dome,dome]
+const pack_9 = [tentacle,tentacle,dome,dome]
+const pack_10 = [blob,tentacle,dome]
+
+const pack_11 = [orange,orange,blob,blob,blob,blob,dome,dome]
+const pack_12 = [orange,orange,blob,blob,dome,dome,dome,dome]
+const pack_13 = [amogus,amogus,tentacle,tentacle,dome,dome,dome,dome]
+const pack_14 = [flower,flower,tentacle,tentacle,blob,blob,blob,blob]
+const pack_15 = [ghost,ghost]
+const pack_16 = [ghost,ghost,ghost]
+const pack_17 = [amogus,amogus]
+const pack_18 = [amogus,amogus,amogus]
+const pack_19 = [orange,orange,orange]
+const pack_20 = [flower,flower,flower]
+
+const pack_21 = [tendrils,tendrils]
+const pack_22 = [tendrils,tendrils,tendrils]
+const pack_23 = [time,time]
+const pack_24 = [time,time,time]
+const pack_25 = [prism]
+const pack_26 = [prism,prism]
+const pack_27 = [prism,prism,prism]
+const pack_28 = [floater]
+const pack_29 = [floater,floater]
+const pack_30 = [floater,floater,floater]
 
 # this is a list of encounters
 # each encounter is in the format [number, list]
@@ -40,10 +73,46 @@ const pack_3 = [dome, dome, dome]
 # a pack will be picked from the list and duplicated (number) times
 # pack options are seen above. they don't need to be 3 mobs
 const encounters = [[0, [[]]], # floor 0 (unused)
-    [1, [pack_3, pack_3]], # floor 1
-    [2, [pack_1, pack_2]], # floor 2
-    [3, [pack_1, pack_2]], # floor 3
-    [1, [pack_3]] # floor 4
+    [1, [pack_1]], # floor 1
+    [1, [pack_2]], # floor 2
+    [1, [pack_4]], # floor 3
+    [1, [pack_7]], # floor 4
+    [1, [pack_6]],
+    [1, [pack_8, pack_9, pack_10]],
+    [2, [pack_1,pack_2,pack_3]],
+    [2, [pack_1,pack_2,pack_3,pack_4,pack_5,pack_6]],
+    [2, [pack_4,pack_5,pack_6,pack_7,pack_8,pack_9]],
+    [3, [pack_4,pack_5,pack_6,pack_10]], #10
+    [1, [pack_11,pack_12]],
+    [2, [pack_7,pack_8,pack_9,pack_19]],
+    [1, [pack_13]],
+    [2, [pack_17,pack_18,pack_19]],
+    [1, [pack_14]],
+    [4, [pack_7,pack_8,pack_9]],
+    [2, [pack_14, pack_20]],
+    [3, [pack_15,pack_17,pack_18,pack_19,pack_20]],
+    [3, [pack_15,pack_16,pack_17,pack_18,pack_19,pack_20]],
+    [3, [pack_16,pack_18,pack_19,pack_20]], #20
+    [3, [pack_16,pack_18,pack_19,pack_20,pack_21]],
+    [3, [pack_16,pack_18,pack_20,pack_21,pack_22]],
+    [4, [pack_23,pack_24]],
+    [3, [pack_16,pack_20,pack_21,pack_22,pack_23,pack_24]],
+    [4, [pack_20,pack_25]],
+    [3, [pack_16,pack_22,pack_24,pack_25,pack_26]],
+    [3, [pack_16,pack_22,pack_24,pack_25,pack_26,pack_27]],
+    [3, [pack_26,pack_27]],
+    [3, [pack_16,pack_22,pack_24,pack_26,pack_27]],
+    [3, [pack_22,pack_24,pack_26,pack_27,pack_28]], #30	
+    [3, [pack_22,pack_24,pack_27,pack_28]],
+    [5, [pack_16]],
+    [4, [pack_22]],
+    [3, [pack_22,pack_24,pack_27,pack_29]],
+    [4, [pack_27]],
+    [4, [pack_24,pack_27,pack_29]],
+    [4, [pack_27,pack_29]],
+    [4, [pack_27,pack_30]],
+    [4, [pack_30]],
+    [5, [pack_30]], #40
     ]
 
 func _ready():
